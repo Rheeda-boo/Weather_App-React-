96d3b79c9d4f00f0df77beb2d1746d5b6b6a250d
Rheeda-boo <rheeda.beecha@gmail.com>
2020-10-11 20:02:00 +0100

    weather app, backgroung image set and styled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const api = {
+  key: "a43375849c950e522f5c40d99bf2f003",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className = "searchBox">
+          <input type = "text" className = "searchBar" placeholder = "Search..."/>
+        </div>
+      </main>
     </div>
   );
 }
