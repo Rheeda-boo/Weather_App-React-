c08e3421c24ada39f3bfaafbabd7405abbfbb72a
Rheeda-boo <rheeda.beecha@gmail.com>
2020-10-11 20:46:04 +0100

    date displayed, display page styled

--- src/App.js
+++ src/App.js
@@ -6,12 +6,44 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month  = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
         <div className = "searchBox">
           <input type = "text" className = "searchBar" placeholder = "Search..."/>
         </div>
+
+        <div className ="locationBox">
+
+          <div className = "location">
+            Airport Ridge, Takoradi
+          </div>
+
+           <div className = "date">{dateBuilder(new Date())}</div>
+
+          <div className = "weatherBox">
+
+            <div className = "temp">
+              15*c
+            </div>
+
+            <div className = "weather">Sunny</div>
+
+          </div>
+        </div> 
       </main>
     </div>
   );
